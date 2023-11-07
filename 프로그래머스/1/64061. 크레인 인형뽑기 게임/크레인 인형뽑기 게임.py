67b7930148c34df70d681102c482b3ee56d8a6c0
Youngseok-O <82360640+Youngs5@users.noreply.github.com>
2023-11-07 16:37:31 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 2.45 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.py
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.py
@@ -1,32 +1,23 @@
 def solution(board, moves):
-    new_board = []
-    fake_board = []
     basket = []
     answer = 0
     
-    for s_board in range(0, len(board)):
-        for t_board in range(len(board)-1, -1, -1):
-            fake_board.append(board[t_board][s_board])
-        new_board.append(fake_board)
-        fake_board = []
-        
-    for i in range(0, len(new_board)):
-        while (new_board[i][-1] == 0):
-            new_board[i].pop()
+    for move in moves:
+        index = 0
+        while index < len(board) and board[index][move - 1] == 0:
+            index += 1
+
+        if index < len(board):
+            if len(basket) == 0:
+                basket.append(board[index][move-1])
+                board[index][move-1] = 0
+            else:
+                if basket[-1] == board[index][move-1]:
+                    basket.pop()
+                    answer += 2
+                    board[index][move-1] = 0
+                else:
+                    basket.append(board[index][move-1])
+                    board[index][move-1] = 0
     
-    for draw in moves:
-        count = draw-1
-        son_board = new_board[count]
-        
-        if len(son_board) == 0:
-            continue
-        
-        if len(basket) != 0 and basket[-1] == son_board[-1]:
-            basket.pop()
-            son_board.pop()
-            answer += 2
-        else:
-            basket.append(son_board[-1])
-            son_board.pop()
-        
     return answer
